cf4e6735d48f18f38ec6becfe3d6ed9c5fb0b3ef
aNovaely-VM <Mardevol.carrat@Gmail.com>
2026-04-30 22:06:23 +0200

    chore: next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  // Logs détaillés en dev
+  ...(process.env.NODE_ENV === 'development' && {
+    onDemandEntries: { maxInactiveAge: 60 * 1000, pagesBufferLength: 5 },
+  }),
+}
 module.exports = nextConfig
